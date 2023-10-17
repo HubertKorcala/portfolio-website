24c6369ae1297d2511b4371ab5ff2fd988c34b43
HubertKorcala <korcalahubert@gmail.com>
2023-10-17 16:05:07 +0200

    feat: create MenuOverlay component

---
+++ src/app/components/MenuOverlay.tsx
@@ -0,0 +1,19 @@
+import NavLink from "./NavLink";
+
+const MenuOverlay = ({
+  links,
+}: {
+  links: { title: string; href: string }[];
+}) => {
+  return (
+    <ul className="flex flex-col py-4 items-center">
+      {links.map((link, index) => (
+        <li key={index}>
+          <NavLink href={link.href} title={link.title} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default MenuOverlay;
