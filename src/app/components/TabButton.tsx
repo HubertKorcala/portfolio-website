225a19d90ac2dba86352bef9b04681b0d53f4d69
HubertKorcala <korcalahubert@gmail.com>
2023-10-18 13:00:58 +0200

    feat: create TabButton

---
+++ src/app/components/TabButton.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const TabButton = ({
+  active,
+  selectTab,
+  children,
+}: {
+  active: boolean;
+  selectTab: () => void;
+  children: React.ReactNode;
+}) => {
+  const buttonClasses = active
+    ? `text-white border-b border-purple-500`
+    : `text-[#ADB7BE]`;
+
+  return (
+    <button onClick={selectTab}>
+      <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
+        {children}
+      </p>
+    </button>
+  );
+};
+
+export default TabButton;
