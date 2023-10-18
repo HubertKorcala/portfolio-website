d125a9558ad0428219b7407c50d75327c31485e0
HubertKorcala <korcalahubert@gmail.com>
2023-10-18 22:15:02 +0200

    fix: delete left console.log for debugging

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -65,8 +65,6 @@ const AboutSection = () => {
     }
   };
 
-  console.log(tabData);
-
   return (
     <section className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
